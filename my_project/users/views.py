8788dcb78d8d48814874085845cf9e8e697314b4
Breathe ESG <harshit@breatheesg.com>
2025-08-27 12:34:11 +0530

    Added get route

--- my_project/users/views.py
+++ my_project/users/views.py
@@ -1,6 +1,14 @@
+from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer
 
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class ListUsersView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    filterset_fields = ['username', 'email']
+
